7488fbf07e66de3d08ed7162eab14d2b44979403
cLoon21 <31352618+cLoon21@users.noreply.github.com>
2017-10-27 00:21:42 -0500

    Created Menus
    Created two menus, one for controls and one for main menu.

---
+++ Assets/Scripts/ControlMenuActions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlMenuActions : MonoBehaviour {
+
+	public void LoadMainMenu(){
+		Application.LoadLevel ("Main_Menu");
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/MainMenuActions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuActions : MonoBehaviour {
+
+	public GameObject Panel;
+
+	public void LoadScene(){
+		Application.LoadLevel ("Stage_One");
+	}
+
+	public void QuitScene(){
+		Application.Quit();
+	}
+
+	public void TurnOffMainMenu(){
+		Application.LoadLevel ("Controls_Menu");
+		//canvasGroup.gameObject.SetActive(!canvasGroup.gameObject.activeSelf);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
