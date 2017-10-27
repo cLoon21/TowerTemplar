e917b6a4c5fb374e37d0dff961705bd974f504e9
ant-tran <ant-tran@users.noreply.github.com>
2017-10-27 04:33:54 -0500

    appropriate name

---
+++ Assets/Scripts/KnightHealth.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightHealth : MonoBehaviour {
+
+	Animator anim;
+
+	public int health = 3;
+	public bool beingDamaged = false;
+	public bool damageCollide = false;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKeyDown (KeyCode.P) && beingDamaged == false) {
+			beingDamaged = true;
+			anim.SetInteger ("Health", health);
+			StartCoroutine (delayTime (2));
+		}
+		if (health == 0) {
+			StartCoroutine (delayTime (1));
+		}
+	}
+		
+	IEnumerator delayTime(int time){
+		yield return new WaitForSeconds (time);
+
+		if (health > 0) {
+			health--;
+		} else if (health == 0) {
+			Application.LoadLevel ("Stage_One");
+		}
+
+		beingDamaged = false;
+	}
+}
