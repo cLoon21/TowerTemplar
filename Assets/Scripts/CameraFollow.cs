dc5647f83427a697741041ae0f84502da007b284
nrlick13 <32905227+nrlick13@users.noreply.github.com>
2017-12-01 22:05:44 -0600

    scene_3 changes

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,14 +6,32 @@ public class CameraFollow : MonoBehaviour {
 
 	public Transform target;
 
+	public Camera camera;
+
 	public float smoothSpeed = 0.15f;
 	public Vector3 offset;
 
+	//deals with boss camera
+	bool bossBounded = false;
+	float bossBoundedRadius = 0.2f;
+	public Transform bossBoundCheck;
+	public LayerMask whatIsBossBound;
+
+	void FixedUpdate (){
+
+		//check if player is in boss area
+		bossBounded = Physics2D.OverlapCircle (bossBoundCheck.position, bossBoundedRadius, whatIsBossBound);
+
+		if (bossBounded) {
+			camera.orthographicSize = 13.5f;
+		}
+	}
 
 	// Update is called once per frame
 	void LateUpdate () {
 		Vector3 desiredPos = target.position + offset;
 		Vector3 smoothedPos = Vector3.Lerp (transform.position, desiredPos, smoothSpeed);
 		transform.position = smoothedPos;
+
 	}
 }
